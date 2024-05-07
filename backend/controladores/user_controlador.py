05e57aacd54c5a1db52df941b5e2e893f054951b
gpanelli3 <gpanelli3@gmail.com>
2024-05-07 20:09:50 -0300

    'faltan-validaciones'

--- backend/controladores/user_controlador.py
+++ backend/controladores/user_controlador.py
@@ -2,13 +2,29 @@ from flask import Blueprint, request, current_app, jsonify,current_app
 from models.modelUser import UserModel
 import json
 from flask_cors import CORS
+import pymongo 
+
+db= pymongo.MongoClient("mongodb://localhost:27017")
+mydb=db["lastNews"]
+coleccion=mydb["users"]
+
 
 user_bp = Blueprint('user', __name__, url_prefix='/user')
-
 @user_bp.post("/createUser")
 def create_user():
-    data = request.json
 
+    data = request.json
+    print("email form", data["email"])
+
+    #traer todos los usuarios y comparar si existe el mail
+    documentos={}
+    users=coleccion.find()
+    
+    for documentos in users:# la idea es comparar el diccionario que se forma con el FORM (data) y el diccionario en la base de datos
+        if data["email"] == documentos["email"]:
+            print("mismo email, ya tenes usuario")
+            break
+        
     name = data["name"]
     user = data["user"]
     password = data["password"]
@@ -25,4 +41,6 @@ def create_user():
     
     except Exception as e:
         return jsonify({e})
+    
+
 
