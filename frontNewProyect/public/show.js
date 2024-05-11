b5b2c7ade19f4868ea03bbe7ef4fc63e05ed54fc
gpanelli3 <gpanelli3@gmail.com>
2024-05-11 18:36:49 -0300

    'cards'

--- frontNewProyect/public/show.js
+++ frontNewProyect/public/show.js
@@ -1,3 +1,4 @@
+crear=document.querySelector('#crearNoticia');
 document.addEventListener('DOMContentLoaded', () => {
     fetch('http://localhost:5000/news/showNews', {
         method: 'GET',
@@ -16,16 +17,34 @@ document.addEventListener('DOMContentLoaded', () => {
                 noticiasDiv.appendChild(row);
             }
             const noticiaElement = document.createElement('div');
+
             noticiaElement.classList.add('col');
             const titulo = document.createElement('h2');
             titulo.textContent = noticia.titulo;
+            noticiaElement.classList.add('mi-clase-css');
+            titulo.classList.add('titulo');
             noticiaElement.appendChild(titulo);
+
             const enlace = document.createElement('a');
             enlace.textContent = 'Leer';
-            enlace.href = '/news/viewNews/' + noticia._id.$oid;
+            enlace.href = 'http://127.0.0.1:5500/lastNews/frontNewProyect/public/news/viewNews/' + noticia._id.$oid;
+            enlace.classList.add('titulo');
             noticiaElement.appendChild(enlace);
             row.appendChild(noticiaElement);
+            
+            const fecha=document.createElement('p')
+            fecha.textContent = noticia.fecha
+            fecha.classList.add('bottom');
+            noticiaElement.appendChild(fecha);
         });
     })
     .catch(error => console.error('Error:', error));
+});
+
+
+
+crear.addEventListener('click', (e) =>{
+    //e.preventDefault();
+    window.location.href = "http://127.0.0.1:5500/lastNews/frontNewProyect/public/newNews.html"
+
 });
